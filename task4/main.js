a061b2e48f7e8e730acc4eef3e49289fe3b3dd36
JerryUusis <jerry.uusitalo@gmail.com>
2023-08-29 14:33:32 +0300

    Added functionality on main.js on caluclator and displaying results on the page

--- task4/main.js
+++ task4/main.js
@@ -1,7 +1,12 @@
-const price = document.querySelector("#price");
-const money = document.querySelector("#money");
+function calcGasoline() {
+const price = document.querySelector("#price").value;
+const money = document.querySelector("#money").value;
+const answer = document.getElementById("sum");
 
+division = (money / price).toFixed(2);
 
+console.log(price);
+console.log(money);
 
-console.log(price)
-console.log(money)
+division >= 10 ? answer.innerText = `You get ${division} litres of gas. Good, you can escape now` : answer.innerText = `You get ${division} litres of gas. Ups, you have to stay here.`;
+}
