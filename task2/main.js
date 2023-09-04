f79a63b8235d21f7b6bbd73d2593166fdb5b2441
JerryUusis <jerry.uusitalo@gmail.com>
2023-09-04 10:42:01 +0300

    Task 2: Updated the exercises as comments on main.js

--- task2/main.js
+++ task2/main.js
@@ -26,6 +26,16 @@
 //         break;
 // }
 
+/* TASK
+
+Make a program that asks about hourly salary and how many hours the user worked in one day. With this information, the program calculates the day’s salary.
+
+The salary is calculated as follows:
+ - first 7 hours by hourly salary
+ - next 2 hours, 50% extra
+ - rest of the hours 100% extra */
+
+
 //Variable definitions for task2
 
 // let hourlySalary = parseFloat(prompt("What is your hourly salary?"));
