6804e773451875a436115c22e28514070f8a3f22
JerryUusis <jerry.uusitalo@gmail.com>
2023-09-11 14:14:47 +0300

    Insurance app: Added DOM variables in JS and fitting attributes for inputs in HTML

--- insurance_app/main.js
+++ insurance_app/main.js
@@ -1,1 +1,12 @@
 //DOM variables
+let riskScore = 500;
+const firstName = document.querySelector("#first-name");
+const lastName = document.querySelector("#last-name");
+const userAge = document.querySelector("#age");
+const currentHealthCheckBoxes = document.querySelectorAll(".current-health");
+const healthyCheckBoxes = document.querySelectorAll(".healthy-habits");
+const unHealthyCheckBoxes = document.querySelectorAll(".unhealthy-habits");
+
+
+//Functions
+
